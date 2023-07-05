5013c18bb3c78f3621dd1f6154ea061f4a7bac8c
unesco-iesalc <94866118+unesco-iesalc@users.noreply.github.com>
2023-07-05 15:59:51 +0200

    Create version.php

---
+++ version.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_listplugins';
+$plugin->version = 2022111002;
+$plugin->release = 'v1.0.0';
+$plugin->requires = 2019111800;
+$plugin->maturity = MATURITY_ALPHA;
